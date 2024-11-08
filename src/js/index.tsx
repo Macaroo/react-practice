56f6f24597e5743357cf3baf67a470799cf7ae5c
Macaroo <kmacaro67j2@gmail.com>
2024-11-08 14:02:01 +0900

    ChakraUIインストール

--- src/js/index.tsx
+++ src/js/index.tsx
@@ -3,10 +3,13 @@ import { createRoot } from 'react-dom/client';
 import "../css/main.css";
 import { App } from './App';
 import { AuthProvider } from './contexts/AuthContexts';
+import { ChakraProvider } from "@chakra-ui/react";
 
 const root = createRoot(document.getElementById('app')!);
 root.render(
-    <AuthProvider>
-        <App />
-    </AuthProvider>
+    <ChakraProvider>
+        <AuthProvider>
+            <App />
+        </AuthProvider>
+    </ChakraProvider>
 );
